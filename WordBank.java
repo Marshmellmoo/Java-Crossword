6ba26c8181a902f4debdf1281a31bc2931c50639
adsur01 <adsurschool@Gmail.com>
2024-05-29 10:23:59 -0400

    Update WordBank.java

--- WordBank.java
+++ WordBank.java
@@ -1,27 +1,17 @@
-
 public class WordBank {
-
 	private String word;
 	private String hint;
 	
 	public WordBank(String word, String hint) {
-		
 		this.word = word;
 		this.hint = hint;
-		
-		
 	}
 	
 	public String getWord() {
-		
 		return word;
-		
 	}
 	
 	public String getHint() {
-		
-		return hint;
-		
+		return hint;	
 	}
-	
 }
