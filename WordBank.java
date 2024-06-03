52da528f824d4e07df2687268d41ca587a89fcc9
Marshmellmoo <91917705+Marshmellmoo@users.noreply.github.com>
2024-06-03 11:47:15 -0400

    Add files via upload
    new ver

--- WordBank.java
+++ WordBank.java
@@ -1,17 +1,87 @@
+
 public class WordBank {
+
 	private String word;
 	private String hint;
+	private int row;
+	private int col;
+	private boolean isHorizontal;
+	private boolean isVertical;
 	
 	public WordBank(String word, String hint) {
+		
 		this.word = word;
 		this.hint = hint;
+		row = 0;
+		col = 0;
+		isHorizontal = false;
+		isVertical = false;
+		
+		
 	}
 	
 	public String getWord() {
+		
 		return word;
+		
 	}
 	
 	public String getHint() {
-		return hint;	
+		
+		return hint;
+		
 	}
+	
+	public int getRow()  {
+		
+		return row;
+		
+	}
+	
+	public void setRow(int num) {
+		
+		row = num;
+		
+	}
+	
+	public int getCol() {
+		
+		return col;
+		
+	}
+	
+	public void setCol(int num) {
+		
+		col = num;
+		
+		
+	}
+	
+	public boolean isHorizontal() {
+		
+		return isHorizontal;
+		
+	}
+	
+	public void changeHorizontal() {
+		
+		if (isHorizontal == false) isHorizontal = true;
+		else isHorizontal = false;
+		
+	}
+	
+	public boolean isVertical() {
+		
+		return isVertical;
+		
+		
+	}
+	
+public void changeVertical() {
+		
+		if (isVertical == false) isVertical = true;
+		else isVertical = false;
+		
+	}
+	
 }
