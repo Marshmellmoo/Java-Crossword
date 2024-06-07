f81edb17c1e9d9bef682e0f726da89b9088f0e77
Marshmellmoo <91917705+Marshmellmoo@users.noreply.github.com>
2024-06-07 09:55:06 -0400

    Add files via upload

---
+++ Words.java
@@ -0,0 +1,26 @@
+
+public class Words {
+
+	private String word;
+	private String hint;
+	
+	public Words(String word, String hint) {
+		
+		this.word = word;
+		this.hint = hint;
+		
+	}
+	
+	public String getWord() {
+		
+		return word;
+		
+	}
+	
+	
+	public String getHint() {
+		
+		return hint;
+		
+	}
+}
