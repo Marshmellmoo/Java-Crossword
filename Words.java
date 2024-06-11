0db3b7f9db8caf236f04fd2d724ea7fbc3b4ef39
adsur01 <adsurschool@Gmail.com>
2024-06-11 00:14:53 -0400

    Update Words.java

--- Words.java
+++ Words.java
@@ -1,26 +1,17 @@
-
 public class Words {
-
 	private String word;
 	private String hint;
 	
 	public Words(String word, String hint) {
-		
 		this.word = word;
 		this.hint = hint;
-		
 	}
 	
 	public String getWord() {
-		
 		return word;
-		
 	}
 	
-	
-	public String getHint() {
-		
+	public String getHint() {	
 		return hint;
-		
 	}
 }
