f1dde9b834b924b07b037c267c93d29f6e0090a8
adsur01 <adsurschool@Gmail.com>
2024-06-11 02:45:46 -0400

    Update CheckingOutput.java

--- CheckingOutput.java
+++ CheckingOutput.java
@@ -1,26 +1,22 @@
 public String checkAnswer(String[][] inputCrossword) {
-	boolean check = true; 
-	int mistakeCounter = 0;
-	//2D array for answer key grid (check)
-	String ansKey[][] = new String[5][5]; //unfinished 
-	
-	for (int row = 0; row < ansKey.length; row++) { //replace size with row length of grid
-    	for (int col = 0; col < ansKey[0].length; col++) {
-    		if (inputCrossword[row][col].equals(ansKey[row][col])) {
-    			check = true; 
-    		}
-    		
-    		else {
-    			check = false;
-    			mistakeCounter++; 
-    		}
-    		}
-    	}
+    boolean check = true;
+    int mistakeCounter = 0;
+    String[][] ansKey = crossword.getGrid(); // assuming the grid contains the correct answers
 
-	if (check == true) {
-		System.out.println("Congratulations! You correctly completed the crossword puzzle.");
-        } 
-	else {
-		System.out.println("Incorrect. Try again. You have " + mistakeCounter + "mistakes.");
+    for (int row = 0; row < ansKey.length; row++) { 
+        for (int col = 0; col < ansKey[0].length; col++) {
+            if (inputCrossword[row][col].equals(String.valueOf(ansKey[row][col]))) {
+                check = true;
+            } else {
+                check = false;
+                mistakeCounter++;
+            }
         }
-}}
+    }
+
+    if (check) {
+        return "Congratulations! You correctly completed the crossword puzzle.";
+    } else {
+        return "Incorrect. Try again. You have " + mistakeCounter + " mistakes.";
+    }
+}
