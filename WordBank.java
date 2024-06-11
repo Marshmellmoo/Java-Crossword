ed29a84af06238696abe3e809f6fc74df728b379
adsur01 <adsurschool@Gmail.com>
2024-06-11 00:17:40 -0400

    Update WordBank.java

--- WordBank.java
+++ WordBank.java
@@ -1,81 +1,53 @@
-
 public class WordBank {
-
 	private Words text;
 	private int row;
 	private int col;
 	private boolean isVertical;
 	
 	public WordBank(Words index) {
-		
 		this.text = index;
 		row = 0;
 		col = 0;
-		isVertical = false;
-		
-		
+		isVertical = false;	
 	}
 	
 	public Words getText() {
-		
 		return text;
-		
 	}
 	
 	public void setText(Words text) {
-		
 		this.text = text;
-		
 	}
 	
 	public String getWord() {
-		
 		return text.getWord();
-		
 	}
 	
 	public String getHint() {
-		
 		return text.getHint();
-		
 	}
 	
 	public int getRow()  {
-		
 		return row;
-		
 	}
 	
 	public void setRow(int num) {
-		
 		row = num;
-		
 	}
 	
 	public int getCol() {
-		
 		return col;
-		
 	}
 	
 	public void setCol(int num) {
-		
 		col = num;
-		
-		
 	}
 	
 	public boolean isVertical() {
-		
-		return isVertical;
-		
-		
+		return isVertical;		
 	}
 	
-	public void changeVertical(boolean value) {
-		
+	public void changeVertical(boolean value) {	
 		isVertical = value;
-		
 	}
-	
 }
